c184bb8b6d8af9e10816ea1ce7160973b81062ba
Pop-Ace <gbengapopoola01@yahoo.com>
2023-02-16 09:04:46 -0800

    printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints an output using printf
+ * return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
