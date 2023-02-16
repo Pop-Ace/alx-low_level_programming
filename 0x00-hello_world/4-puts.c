d71575ffec1d4b2d7aed54c5253256e1543f279b
Pop-Ace <gbengapopoola01@yahoo.com>
2023-02-16 08:34:30 -0800

    hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program that Prints a line using the puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
