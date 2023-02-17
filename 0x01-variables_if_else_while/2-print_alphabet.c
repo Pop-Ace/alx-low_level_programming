dc277d73c6c99c3dd41f6314d562b228c4aac00d
Pop-Ace <gbengapopoola01@yahoo.com>
2023-02-17 10:32:55 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: A program that print the lowercase alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
