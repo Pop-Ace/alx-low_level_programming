26d7144caca264234784be5508fef87d5842a902
Pop-Ace <gbengapopoola01@yahoo.com>
2023-02-17 10:54:41 -0800

    lower and UPPER alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Alphabet Game - lowecase and UPPERCASE
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
