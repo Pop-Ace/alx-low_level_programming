5d2cb87dbba57b03e76f95399e3b953b9a761d1e
Pop-Ace <gbengapopoola01@yahoo.com>
2023-02-21 14:29:56 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,38 +5,25 @@
  * Description: Prints 9 times table
  * Return: void
  */
+void times_table(void) {
+    int i, j, product;
 
-void times_table(void)
-{
-	int i, j, ans, tens, unit;
-
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			ans = i * j;
-			tens = ans / 10;
-			unit = ans % 10;
-			if (j == 0)
-			{
-				_putchar('0');
-
-			}
-			else if (ans < 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(ans + ' ');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(tens / 10 + '0');
-				_putchar(unit % 10 + '0');
-			}
-		}
-		_putchar('\n');
-	}
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= 9; j++) {
+            product = i * j;
+            if (product < 10) {
+                _putchar(' ');
+                _putchar(product + '0');
+            } else {
+                _putchar((product / 10) + '0');
+                _putchar((product % 10) + '0');
+            }
+            if (j < 9) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
 }
+
