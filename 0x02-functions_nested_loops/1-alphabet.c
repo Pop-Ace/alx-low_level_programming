946fbc81d23210179c6351d1b31e72dcc88b7aab
Pop-Ace <gbengapopoola01@yahoo.com>
2023-02-21 07:42:51 -0800

    lowercase alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description - Printing alphabet in lowercase
+ * Return:(void)
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+_putchar('\n');
+}
