9ed3b4c817b94c3f5b1e966964c9e2362f185bef
Pop-Ace <gbengapopoola01@yahoo.com>
2023-02-27 06:09:09 -0800

    Length of a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _POINTERS_MAIN_H
+#define _POINTERS_MAIN_H
 
+#include <unistd.h>
 #include <stdio.h>
 
+void _putchar(char c); /* TODO: Check if this is the correct prototype */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,6 +14,8 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+
+int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 
-#endif /* MAIN_H */
+#endif /* _POINTERS_MAIN_H */
