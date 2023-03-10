4e51f426f88d5719eae84e4f236bb17d743c6bf9
Pop-Ace <gbengapopoola01@yahoo.com>
2023-03-10 08:20:59 -0800

    Task 2: printing all arguments received

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,21 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives
- * @argc: number of arguments
- * @argv: array of arguments
- *
+ * main - Entry point
+ * Description: Prints all arguments it receives
+ * @argc: No of command line arguments
+ * @argv: Array name
  * Return: 0
  */
-int main(__attribute__((unused)) int argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
+	{
 		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
