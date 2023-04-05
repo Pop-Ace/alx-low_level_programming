2be52ed3ee4b6f52c8f4a7f6e0d55bee982087ef
Pop-Ace <gbengapopoola01@yahoo.com>
2023-04-05 04:53:48 -0700

    Task 4: freeing a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - Entry point
+ * Description: Frees a listint_t list.
+ * @head: Pointer to the head node in the linked list
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
