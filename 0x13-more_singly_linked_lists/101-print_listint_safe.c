29eb4688ce1d096ba886f60af8ddf051e2f892de
Pop-Ace <gbengapopoola01@yahoo.com>
2023-04-06 15:31:02 -0700

    Fix: code

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,35 +3,35 @@
 /**
  * print_listint_safe - Prints a listint_t linked list.
  * @head: Point to the first node of the linked list
- * Return: The number of nodes in the list
+ * Return: Number of nodes in the list
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-        const listint_t *prev, *next;
-        size_t total = 0;
+	const listint_t *prev, *next;
+	size_t total = 0;
 
-        if (!head || !head->next)
-        {
-                return (0);
-        }
-        /* Traversing the linked list */
-        prev = head;
-        while (prev)
-        {
-                printf("[%p] %d\n", (void *)prev, prev->n);
-                total++;
-        /* If next pointer points to previous, break the loop */
-                if (prev > prev->next)
-                {
-                        prev = prev->next;
-                }
-                else
-                {
-                        next = prev->next;
-                        printf("-> [%p] %d\n", (void *)next, next->n);
-                        break;
-                }
-        }
-        return (total);
+	if (!head || !head->next)
+	{
+		return (0);
+	}
+	/* Traversing the linke list */
+	prev = head;
+	while (prev)
+	{
+		printf("[%p] %d\n", (void *)prev, prev->n);
+		total++;
+		/* Break loop if next pointer points to previous node */
+		if (prev > prev->next)
+		{
+			prev = prev->next;
+		}
+		else
+		{
+			next = prev->next;
+			printf("-> [%p] %d\n", (void *)next, next->n);
+			break;
+		}
+	}
+	return (total);
 }
