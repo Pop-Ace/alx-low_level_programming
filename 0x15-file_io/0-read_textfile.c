a299452427b0a7c7c9f8fb6a0dcd2caeb73aa89f
Pop-Ace <gbengapopoola01@yahoo.com>
2023-04-12 16:41:15 -0700

    Fix: Task 1 code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,50 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
- * Return: number of letters it could read and print
- */
+* read_textfile - To read the text file
+* @filename: Name of the file to be read
+* @letters: The number of letters it should read and print
+*
+* Return: The actual number of letters it could read and print
+* if the file can not be opened or read, return 0
+* if filename is NULL return 0
+* if write fails or does not write the expected amount of bytes, return 0
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buf;
-	ssize_t read_bytes, write_bytes;
+int fildes;
+char *buffer;
+ssize_t read_context, write_context;
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL || filename == NULL)
-		return (0);
+buffer = malloc(sizeof(char) * letters);
+if (!buffer || !filename)
+return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buf);
-		return (0);
-	}
+fildes = open(filename, O_RDONLY);
+if (fildes == -1)
+{
+free(buffer);
+return (0);
+}
 
-	read_bytes = read(fd, buf, letters);
-	if (read_bytes == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+read_context = read(fildes, buffer, letters);
+if (read_context == -1)
+{
+free(buffer);
+close(fildes);
+return (0);
+}
 
-	write_bytes = write(STDOUT_FILENO, buf, read_bytes);
-	if (write_bytes == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+write_context = write(STDOUT_FILENO, buffer, read_context);
+if (write_context == -1)
+{
+free(buffer);
+close(fildes);
+return (0);
+}
 
-	free(buf);
-	close(fd);
-	return (write_bytes);
+free(buffer);
+close(fildes);
+return (write_context);
 }
